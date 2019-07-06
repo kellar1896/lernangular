6eff9e56a9e3421a45e8d387689a28e6ac4c1565
cristian <cgbarretor@correo.udistrital.edu.co>
2019-07-06 17:56:20 -0500

    se crean las primeras clases y metodos del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myfirstapp';
+  users = ['Cristian', 'Raquel', 'Nestor', 'Felipe', 'Perrito'];
+  activated = true;
+  name: string = 'Cristian';
+  age: number;
+  address: {
+    street: string,
+    city: string
+  };
+  hobbies: string[];
+
+  constructor(){
+    this.age = 22;
+    this.address = {
+      street: 'cll 29 sur # 50b - 06',
+      city: 'bogota'
+    };
+    this.hobbies = ['nadar', 'programar', 'comer'];
+  }
+
+  saludo() {
+    alert("surprise motherfickers")
+  }
+
+  deleteuser(user){
+    for(let i = 0; i < this.users.length; i++ ){
+      if (user == this.users[i]) {
+        this.users.splice(i, 1);
+      }
+    }
+
+  }
 }
